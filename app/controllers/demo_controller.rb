fe8163892ed60a9efc2cf86e3be8fb4a1dc154e0
Jose Antonio Torres Garibay <pepe_torres1998@hotmail.com>
2024-08-11 23:37:12 -0600

    Add panorama demo page

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,3 @@
+class DemoController < ApplicationController
+  def panorama; end
+end
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import DemoController from "./demo_controller"
+application.register("demo", DemoController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
